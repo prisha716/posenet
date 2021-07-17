b9604d84bb205deabf72defae9477e6dfe86f9d6
prisha716 <84237696+prisha716@users.noreply.github.com>
2021-07-17 17:35:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ function preload() {
 }
  function setup() {
      video=createCapture(VIDEO);
-     video.size(500,450);
+     video.size(500,300);
      video.hide();
-     canvas=createCanvas(500,450);
+     canvas=createCanvas(500,300);
      canvas.center();
      poseNet=ml5.poseNet(video,modelLoaded);
      poseNet.on('pose',gotPoses);
