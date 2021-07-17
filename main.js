2d78eb1676480b2f1d27a3a87a591949a528c1c0
prisha716 <84237696+prisha716@users.noreply.github.com>
2021-07-17 17:29:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightwristX=0;
+leftwristX=0;
+function preload() {
+}
+ function setup() {
+     video=createCapture(VIDEO);
+     video.size(550,500);
+     video.hide();
+     canvas=createCanvas(550,500);
+     canvas.center();
+     poseNet=ml5.poseNet(video,modelLoaded);
+     poseNet.on('pose',gotPoses);
+ }
+ function modelLoaded() {
+     console.log('PoseNet is started');
+ }
+ function gotPoses(results) {
+     
+  if(results.length > 0){}
+    }
+    function draw() {
+    background('#c495c0');
+    }
